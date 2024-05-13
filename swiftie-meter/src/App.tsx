289f4b0f352b1b2470d4721b89a0f807667d6ed2
Aman Purohit <purohiam@sheridancollege.ca>
2024-05-13 12:50:09 -0400

    Started stayling the Trivia Component

--- swiftie-meter/src/App.tsx
+++ swiftie-meter/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Swiftie Meter</h1>
+        {/* <h1>Swiftie Meter</h1> */}
         <Trivia />
       </div>
     </>
--- swiftie-meter/src/components/Trivia/Trivia.tsx
+++ swiftie-meter/src/components/Trivia/Trivia.tsx
@@ -1,80 +1,5 @@
-// import React, { useState } from "react";
-// import data from "../../gai-api/response.json";
-
-// interface TriviaItem {
-//   question: string;
-//   options: {
-//     a: string;
-//     b: string;
-//     c: string;
-//   };
-//   answer: string;
-//   evidence: string;
-// }
-
-// const Trivia = () => {
-//   const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
-//   const [result, setResult] = useState<string[]>([]);
-
-//   const handleOptionChange = (optionKey: string, index: number) => {
-//     const newSelectedOptions = [...selectedOptions];
-//     newSelectedOptions[index] = optionKey;
-//     setSelectedOptions(newSelectedOptions);
-//   };
-
-//   const handleSubmit = (item: TriviaItem, key: number) => {
-//     if (selectedOptions[key] != null) {
-//       if (selectedOptions[key] === item.answer) {
-//         const newResult = [...result];
-//         newResult[key] = `Correct! ${item.evidence}`;
-//         setResult(newResult);
-//       } else {
-//         const newResult = [...result];
-//         newResult[key] = `Incorrect! ${item.evidence}`;
-//         setResult(newResult);
-//       }
-//     } else {
-//       const newResult = [...result];
-//       newResult[key] = `Select your answer!`;
-//       setResult(newResult);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <button type="submit">Start</button>
-//       {data.map((subArray: TriviaItem[], index: number) =>
-//         subArray.map((item: TriviaItem, key: number) => (
-//           <div className="trivia" key={key}>
-//             <h2>Question: {item.question.toUpperCase()}</h2>
-//             <h3>Options</h3>
-//             <label htmlFor={"a"} >a</label>
-//             <button id={"a"} onClick={() => handleOptionChange("a", key)}>
-//               {item.options.a}
-//             </button>
-//             <label htmlFor={"b"} >b</label>
-//             <button id={"b"}  onClick={() => handleOptionChange("b", key)}>
-//               {item.options.b}
-//             </button>
-//             <label htmlFor={"c"} >c</label>
-//             <button id={"c"}  onClick={() => handleOptionChange("c", key)}>
-//               {item.options.c}
-//             </button>
-//             <button type="submit" onClick={() => handleSubmit(item, key)}>
-//               Submit Answer
-//             </button>
-//             <p> You Selected: {selectedOptions[key]}</p>
-//             <p> Result: {result[key]} </p>
-//           </div>
-//         ))
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Trivia;
-import React, { useState } from 'react'
-import data from  '../../gai-api/response.json';
+import React, { useState } from "react";
+import data from "../../gai-api/response.json";
 
 interface TriviaItem {
   question: string;
@@ -91,7 +16,8 @@ const Trivia = () => {
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const [result, setResult] = useState<string | null>(null);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
-  const [noOfQuaestion, setNoOfQuestion] = useState<number>(data.flat().length);
+  const [noOfQuestion, setNoOfQuestion] = useState<number>(data.flat().length);
+  const [noOfCorrect, setNoOfCorrect] = useState<number>(0);
 
   const handleOptionChange = (optionKey: string, key: number) => {
     setSelectedOption(optionKey);
@@ -101,6 +27,7 @@ const Trivia = () => {
     if (selectedOption != null) {
       if (selectedOption === item.answer) {
         setResult(`Correct! ${item.evidence}`);
+        setNoOfCorrect(noOfCorrect + 1);
       } else {
         setResult(`Incorrect! ${item.evidence}`);
       }
@@ -117,20 +44,70 @@ const Trivia = () => {
 
   const item = data.flat()[currentQuestionIndex];
 
+  const totalScore =
+    Number(((noOfCorrect / noOfQuestion) * 100).toPrecision(1)) * 13;
   return (
-    <div>
-      <h2>No. of Questions: {noOfQuaestion- currentQuestionIndex}</h2>
-      <h2>Question: {item.question.toUpperCase()}</h2>
-      <h3>Options</h3>
-      <button value={"a"} onClick={() => handleOptionChange('a', currentQuestionIndex)}>{item.options.a}</button>
-      <button value={"b"} onClick={() => handleOptionChange('b', currentQuestionIndex)}>{item.options.b}</button>
-      <button value={"c"} onClick={() => handleOptionChange('c', currentQuestionIndex)}>{item.options.c}</button>
-      <button type="submit" onClick={() => handleSubmit(item, currentQuestionIndex)}>Submit Answer</button>
-      <p> You Selected: {selectedOption}</p>
-      <p> Result: {result} </p>
-      <button onClick={selectedOption ? handleNext : undefined}>Next Question</button>
-    </div>
-  )
-}
+    <>
+      {currentQuestionIndex === noOfQuestion ? (
+        <div>
+          <h2>Quiz Completed</h2>
+          <h2>Your Swiftie Score: {totalScore}</h2>
+        </div>
+      ) : (
+        <div className="container" > 
+          <div className="flex flex-col items-center justify-center float-left p-2">
+            <div>
+              <h2>Question: {`${currentQuestionIndex} / ${noOfQuestion}`}</h2>
+              <h2>Question: {item.question}</h2>
+            </div>
+            <div>
+              <h3>Options</h3>
+              <div>
 
-export default Trivia
+           
+              <button
+               
+                value={"a"}
+                onClick={() => handleOptionChange("a", currentQuestionIndex)}
+              >
+                a{item.options.a}
+              </button>
+              <button
+               
+                value={"b"}
+                onClick={() => handleOptionChange("b", currentQuestionIndex)}
+              >
+                b{item.options.b}
+              </button>
+              <button
+              
+                value={"c"}
+                onClick={() => handleOptionChange("c", currentQuestionIndex)}
+              >
+                c{item.options.c}
+              </button>
+              </div>
+            </div>
+
+            <div>
+              <div>
+              <button onClick={() => handleSubmit(item, currentQuestionIndex)}>
+                Submit Answer
+              </button>
+              <button onClick={handleNext}>Next Question</button>
+              </div>
+             
+              <div>
+                <p> You Selected: {selectedOption}</p>
+                <p> Result: {result} </p>
+              </div>
+            </div>
+          </div>
+          {/* onClick={selectedOption ? handleNext : undefined} */}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Trivia;
